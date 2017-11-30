893c8e84ea90cbe51615a3e41a5f95933f61e9a0
qinyi <qinyi545541118@outlook.com>
2017-11-30 10:14:44 +0800

    墙的处理

--- second/main.cpp
+++ second/main.cpp
@@ -3,7 +3,6 @@
 #include<time.h>
 #include<malloc.h>
 int r, w;
-int stacks[10000];
 int top = 0;
 int tmp, i, j, enterx, entery, leftx, lefty;
 int ** ditu;
@@ -12,18 +11,20 @@ struct information {
 	int y;
 	int lastx;
 	int lasty;
-	int id;
-	int lastid;
-	int flag;
 };
 struct information step[10000];
-struct information road[1000];
+struct information road[10000];
 
 void displayditu(int **ditu){
 	int i, j;
-	for(i = 1; i < r + 1; ++i){
-		for(j = 1; j < w + 1; ++j){
-			printf("%2d", ditu[i][j]) ;
+	for(i = 0; i < r ; ++i){
+		for(j = 0; j < w; ++j){
+			if(ditu[j][i] == 1){
+				printf("▇");
+			} 
+			else{
+				printf("  ");
+			}
 		}
 		printf("\n");
 	}
@@ -51,7 +52,7 @@ void filestar(int **ditu){
         char name[20];
         printf("请输入地图名字：");
         scanf("%s", name);
-        fp = fopen(name, "w+");
+        fp = fopen(name, "r");
         fscanf(fp, "%d %d %d %d %d %d\n",&r, &w, &enterx, &entery, &leftx, &lefty);
         ditu = (int **)malloc(sizeof(int *) * r);
         for(i = 0; i < w; ++i){
@@ -81,61 +82,118 @@ void starditu(void){
     }
     enterx = 1;
     entery = 1;
-    road[1].x = 1;
-    road[1].y = 1;
+	leftx = r;
+	lefty = w;
+    road[1].x = 0;
+    road[1].y = 0;
     road[1].lastx = -1;
     road[1].lasty = -1;
-    road[1].lastid = -1;
-    road[1].id = 1;
-    road[1].flag = 1;
+    ditu[0][0] = 0;
 
 }
 
 void shengchengmigong(){
-    for(i = 2; i < 1000;++i){  //最后复制此代码，设计出5条通向死路的路径作为干扰
-    if(road[i].flag != 1){
-        road[i].flag = 1;
-        road[i].lastid = i - 1;
-        road[i].lastx = road[i-1].x;
-        road[i].lasty = road[i-1].y;
-        srand(time(NULL));
-        int tmp = rand() % 4;
-        tmp++;
-        switch(tmp){
-            case 1:
-                if(road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) {
-                road[i].x = road[i].lastx;
-                road[i].y = road[i].lasty - 1;
-                ditu[road[i].x][road[i].y] = 0;
-                }
-            break;
-            case 2:
-                if(road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) {
-                    road[i].x = road[i].lastx + 1;
-                    road[i].y = road[i].lasty;
-                    ditu[road[i].x][road[i].y] = 0;
-                }
-            break;
-            case 3:
-                if(road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1) {
-                    road[i].x = road[i].lastx;
-                    road[i].y = road[i].lasty + 1;
-                    ditu[road[i].x][road[i].y] = 0;
-                }
-            break;
-            case 4:
-                if(road[i].lastx - 1 >=  0 && ditu[road[i].lastx - 1][road[i].lasty] == 1) {
-                    road[i].x = road[i].lastx - 1;
-                    road[i].y = road[i].lasty;
-                    ditu[road[i].x][road[i].y] = 0;
-                }
-            break;
-        }
-    }
-    if(road[i].x == leftx && road[i].y == lefty){
-        break;
-    }
-}
+	int check = 0;
+	i = 2;
+	srand(time(NULL));
+    for(check = 0; check < 1000; ++check){
+		road[i].lastx = road[i-1].x;
+ 		road[i].lasty = road[i-1].y;				  //最后复制此代码，设计出5条通向死路的路径作为干扰
+	    if((road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) || (road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) || (road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1)){	        
+	        
+	        int tmp = rand() % 4;
+	        tmp++;
+	        switch(tmp){
+	            case 1:
+	                if(road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) {
+		                road[i].x = road[i].lastx;
+		                road[i].y = road[i].lasty - 1;
+		                ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx + 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+                  
+	                }else if(road[i].lastx - 1 >=  0 && ditu[road[i].lastx - 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx - 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1) {
+	                    road[i].x = road[i].lastx;
+	                    road[i].y = road[i].lasty + 1;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }
+	            break;
+	            case 2:
+	                if(road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx + 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lastx - 1 >=  0 && ditu[road[i].lastx - 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx - 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) {
+		                road[i].x = road[i].lastx;
+		                road[i].y = road[i].lasty - 1;
+		                ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1) {
+	                    road[i].x = road[i].lastx;
+	                    road[i].y = road[i].lasty + 1;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }
+	            break;
+	            case 3:
+	                if(road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1) {
+	                    road[i].x = road[i].lastx;
+	                    road[i].y = road[i].lasty + 1;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lastx - 1 >=  0 && ditu[road[i].lastx - 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx - 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) {
+		                road[i].x = road[i].lastx;
+		                road[i].y = road[i].lasty - 1;
+		                ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx + 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }
+	            break;
+	            case 4:
+	                if(road[i].lastx - 1 >=  0 && ditu[road[i].lastx - 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx - 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty - 1 >= 0 && ditu[road[i].lastx][road[i].lasty - 1] == 1) {
+		                road[i].x = road[i].lastx;
+		                road[i].y = road[i].lasty - 1;
+		                ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lastx + 1 < w && ditu[road[i].lastx + 1][road[i].lasty] == 1) {
+	                    road[i].x = road[i].lastx + 1;
+	                    road[i].y = road[i].lasty;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }else if(road[i].lasty + 1 < r && ditu[road[i].lastx][road[i].lasty + 1] == 1) {
+	                    road[i].x = road[i].lastx;
+	                    road[i].y = road[i].lasty + 1;
+	                    ditu[road[i].x][road[i].y] = 0;
+	                }
+	            break;
+	        }
+	        displayditu(ditu);
+	    }else {
+            i--;
+            continue;
+	    }
+	    if(road[i].x == leftx - 1 && road[i].y == lefty - 1){
+	        return;
+	    }
+		++i;
+
+	}
+
 }
 
 void searchroad(void){
@@ -144,8 +202,6 @@ void searchroad(void){
     }
 }
 int main(void){
-	//第一步，开始新建地图
-
 		printf("请问是从文件导入地图还是新建一个地图？文件导入请输入1，新建地图请输入2\n");
 		scanf("%d", &tmp);
 		switch(tmp){
@@ -154,6 +210,7 @@ int main(void){
 				break;
 			case 2:
 				starditu();
+				displayditu(ditu);
                 shengchengmigong();
 		}
 		displayditu(ditu);
